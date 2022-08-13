08efce16f8fea9e59b45fd496fd4606ffa09e01d
Ether186 <evancurtis186@protonmail.com>
2022-08-12 17:56:56 -0700

    Roughdraft for basic css/html finished

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-const express = require("express")
-const fetch = require("isomorphic-fetch")
+// const express = require("express")
+// const fetch = require("isomorphic-fetch")
+
+console.log("test");
+
+const acc = document.getElementsByClassName("accordion");
+
+acc.addEventListener("click", function () {
+  this.classList.toggle("active");
+  /* Toggle between hiding and showing the active panel */
+  var panel = this.nextElementSibling;
+  if (panel.style.display === "block") {
+    panel.style.display = "none";
+  } else {
+    panel.style.display = "block";
+  }
+});
